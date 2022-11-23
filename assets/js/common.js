19bd44f8908b89560cdf05af3cb97468ccc6d2b8
Leoooo333 <leochenjm@gmail.com>
2022-11-23 19:59:31 +0800

    Test

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('a').removeClass('waves-effect waves-light');
+    $('#news-table').DataTable( {
+        "paging":   true,
+        "ordering": false,
+        "info":     false,
+        "searching": false,
+        "bLengthChange": false,
+        "pageLength": 5,
+        "pagingType": "simple",
+        "language": {
+        "paginate": {
+            "next": '<div class="news-button btn btn-primary btn-sm"><i class="fa fa-caret-right fa-2x"></i></div>',
+            "previous": '<div class="news-button btn btn-primary btn-sm"><i class="fa fa-caret-left fa-2x"></i></div>'  
+          }
+        }
+     } );
 });
